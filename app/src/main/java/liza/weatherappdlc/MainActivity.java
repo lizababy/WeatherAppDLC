9df54166d120915315b1d82f4f1efbbb967eb8d6
lizababy <lizababy88@gmail.com>
2018-06-02 15:12:37 -0400

    optimized code. Used GooglePlaysServices for accuracy and battery optimization.

--- app/src/main/java/liza/weatherappdlc/MainActivity.java
+++ app/src/main/java/liza/weatherappdlc/MainActivity.java
@@ -13,6 +13,7 @@ import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -25,6 +26,16 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.net.InetAddress;
 
 import liza.weatherappdlc.Models.WeatherResponse;
@@ -32,7 +43,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements LocationListener, Callback<WeatherResponse> {
+public class MainActivity extends AppCompatActivity implements Callback<WeatherResponse> {
 
     private static final int REQUEST_PERMISSION_CODE = 101;
     private static final long MIN_TIME_BW_UPDATES = 0;
@@ -40,12 +51,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     TextView cityTV;
     TextView currentTV;
     private WeatherResponse weatherResponse;
-    LocationManager locationManager;
     private Location userLocation;
     private View coordinatedLayoutView;
-    private Criteria criteria;
-    private Context context;
-    private boolean isGpsEnabled;
+    private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,41 +66,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         coordinatedLayoutView = findViewById(R.id.coordinator_layout);
         cityTV = findViewById(R.id.city_textView);
         currentTV = findViewById(R.id.temperature_textView);
-        context = getApplicationContext();
-        locationManager =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         //checking internet connection
         if (!isNetworkConnected()) {
             showMessage("Not Connected to Internet!");
             return;
         }
+        //Getting User Location
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        //get user location
-        enableRuntimePermission();
-        criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, false);
-        checkGpsStatus();
+        getLastKnownLocation();
 
-        if(isGpsEnabled) {
-            if (provider != null) {
-                if (ActivityCompat.checkSelfPermission(
-                        MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        &&
-                        ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                                != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                userLocation = locationManager.getLastKnownLocation(provider);
-                locationManager.requestLocationUpdates(provider, 12000, 7, MainActivity.this);
-            }
-        }else {
-            showMessage("Please Enable GPS First");
-        }
-        //get Weather info with known location
-        if (userLocation != null) {
-            requestWeatherInfo();
-        }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,48 +87,29 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         });
     }
 
-    private void checkGpsStatus() {
+    private void getLastKnownLocation() {
 
-        isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-    }
-
-    private void enableRuntimePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION))
-        {
-
-            showMessage("ACCESS_FINE_LOCATION permission allows us to Access GPS in app");
-        } else {
-
-            checkGpsStatus();
-            if(!isGpsEnabled)
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_CODE);
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //request permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSION_CODE);
+            return;
         }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            userLocation = location;
+                            requestWeatherInfo();
+                        }
+                    }
+                });
     }
 
-    @SuppressLint("MissingPermission")
-    private void requestLocation(){
 
-        //request Permission
-            if(permissionDenied())
-                requestPermission();
-            else
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,(LocationListener)this);
-
-    }
-    private boolean permissionDenied(){
-        showMessage("Checking GPS Permissions...");
-
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
-    }
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSION_CODE);
-    }
-
-    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
@@ -153,8 +119,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     showMessage("Permission Granted, Now your application can access GPS.");
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
+                    getLastKnownLocation();
                 } else {
                     showMessage("Permission Canceled, Now your application cannot access GPS.");
 
@@ -209,15 +174,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         }
     }
 
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d("location", "location changed");
-        userLocation = location;
-
-        requestWeatherInfo();
-    }
-
     private void requestWeatherInfo() {
 
         //make data request - api call
@@ -232,20 +188,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         }
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-        requestWeatherInfo();
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 
     @Override
     public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
